39a5e0b1643025b57994098d84d1f60a9492b6d1
martinbednar <martin.bedn20@gmail.com>
2023-09-14 12:52:11 +0200

    Ignore blank messages.

--- TCPserver.py
+++ TCPserver.py
@@ -2,14 +2,14 @@
 
 from tcpcom import TCPServer
 
-IP_PORT = 22000
+IP_PORT = 3005
 
 def onStateChanged(state, msg):
     if state == "LISTENING":
         print("Server:-- Listening...")
     elif state == "CONNECTED":
         print("Server:-- Connected to", msg)
-    elif state == "MESSAGE":
+    elif state == "MESSAGE" and msg != "":
         print("Server:-- Message received:", msg)
 
 server = TCPServer(IP_PORT, stateChanged = onStateChanged)
